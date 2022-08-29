c6f61a7db52297e54b2fc0adaeec7c05267526a8
Douglas <cbdouglasvirissimo@hotmail.com>
2022-08-29 03:59:19 -0300

    1.6

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/scripts.js
@@ -2,5 +2,7 @@ const myName = "Douglas";
 const birthCity = "Canoas";
 let birthYear = "2003";
 console.log(myName, birthCity, birthYear)
-birthYear = "2030"
+birthYear = "2030";
 console.log(birthYear)
+birthCity = "error";
+console.log(birthCity)
