877188d0f040fdd728a317e3e4f8e409414ca8a3
Douglas <cbdouglasvirissimo@hotmail.com>
2022-08-29 15:48:07 -0300

    7.2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/switch-e-case/scripts.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/switch-e-case/scripts.js
@@ -1,2 +1,17 @@
 
 let candidata = 'aprovada';
+
+switch (candidata) {
+    case 'aprovada':
+        console.log('Está aprovada')
+        break
+    case 'lista':
+        console.log('Está na lista')
+        break
+    case 'reprovada':
+        console.log('Está reprovada')
+        break
+    default:
+        console.log('não se aplica')
+        break
+}
