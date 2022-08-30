ce09c26744b2b365f0fc119cadcb9b33a7f1c593
Douglas <cbdouglasvirissimo@hotmail.com>
2022-08-30 14:19:30 -0300

    E.9

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-01-javascript-primeiros-passos/Exercicios/IX/scripts.js
@@ -0,0 +1,10 @@
+const numA = 10;
+const numB = 15;
+const numC = 18;
+
+let impar = false
+
+if (numA % 2 !== 0 || numB % 2 !== 0 || numC % 2 !== 0) {
+    impar = true;
+};
+    console.log(impar)
