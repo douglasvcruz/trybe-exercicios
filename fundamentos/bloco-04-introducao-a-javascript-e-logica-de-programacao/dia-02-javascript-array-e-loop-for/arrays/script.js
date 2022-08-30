53f590a784cdb4d0847ecbc0480cb7002af25ed4
Douglas <cbdouglasvirissimo@hotmail.com>
2022-08-30 16:07:42 -0300

    1.3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/arrays/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/arrays/script.js
@@ -3,7 +3,13 @@ let menuServices = menu[1];
 
 console.log(menuServices); */
 
-let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+/* let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
 let indexOfPortfolio = menu[2];
 
-console.log(indexOfPortfolio);
+console.log(indexOfPortfolio); */
+
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+
+menu.push("Contato")
+
+console.log(menu);
