f95dcf0d3f0b242ee55ebc92617f2953d2089056
Douglas <cbdouglasvirissimo@hotmail.com>
2022-08-31 01:37:48 -0300

    E.4

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/script.js
@@ -12,6 +12,17 @@ for(let i in numbers) {
 
 console.log(resultado); */ 
 
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+let resultado = 0;
+for(let i in numbers) {
+    resultado += numbers[i];
+};
+
+let media = resultado / numbers.length;
+
+console.log(media); */
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 let resultado = 0;
@@ -21,4 +32,8 @@ for(let i in numbers) {
 
 let media = resultado / numbers.length;
 
-console.log(media);
+if (media > 20) {
+    console.log("valor maior que 20")
+} else {
+    console.log("valor menor ou igual a 20")
+}
