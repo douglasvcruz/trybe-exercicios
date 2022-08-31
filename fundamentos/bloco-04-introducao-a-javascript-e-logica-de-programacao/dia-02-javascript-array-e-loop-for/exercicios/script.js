9a7fe2808ce2dcfe7ebc4b726619f5a19e47fc46
Douglas <cbdouglasvirissimo@hotmail.com>
2022-08-31 01:24:26 -0300

    E.2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/script.js
@@ -1,5 +1,14 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
 for(let i in numbers) {
     console.log(numbers[i])
-}
+} */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+for(let i in numbers) {
+    resultado += numbers[i];
+};
+
+console.log(resultado);
+
