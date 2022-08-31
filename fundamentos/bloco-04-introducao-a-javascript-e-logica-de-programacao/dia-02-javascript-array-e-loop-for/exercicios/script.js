df77019052ee328e30cce73312768efff102f7d0
Douglas <cbdouglasvirissimo@hotmail.com>
2022-08-31 03:30:09 -0300

    E.6

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/exercicios/script.js
@@ -10,7 +10,7 @@ for(let i in numbers) {
     resultado += numbers[i];
 };
 
-console.log(resultado); */ 
+console.log(resultado); */
 
 /* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 
@@ -38,7 +38,7 @@ if (media > 20) {
     console.log("valor menor ou igual a 20")
 } */
 
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+/* let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let maior = numbers[0];
 
 for(i in numbers) {
@@ -47,4 +47,20 @@ for(i in numbers) {
     }
 }
 
-console.log(maior)
+console.log(maior) */
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let resultado = 0;
+
+for (i in numbers) {
+    if (numbers[i] % 2 !== 0) {
+        resultado += 1;
+    }
+}
+
+if(resultado === 0) {
+    console.log("nenhum valor ímpar encontrado");
+} else {
+    console.log(resultado);
+}
+
