f39d1ec5d15d4e17dea9123f610f0d788e10fd28
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-02 14:37:06 -0300

    E.2

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/script.js
@@ -1,7 +1,20 @@
+// 1º Exercício
 let fatorial = 1;
 
-for (let i = 1; i < 11; i += 1) {
+for (let i = 1; i <= 10; i += 1) {
   fatorial = fatorial * i;
 }
 
 console.log(fatorial);
+
+// 2º Exercício
+let word = 'tryber';
+let reverse = ""
+
+for (let i = 0; i < word.length; i += 1) {
+  reverse += word[word.length - 1 - i];
+}
+
+console.log(reverse);
+
+// 3º Exercício
