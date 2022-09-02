281ea6c8a8d59fd3afb3c7cb2430bd41ae87df1b
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-02 15:01:14 -0300

    E.3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/exercicios/script.js
@@ -12,9 +12,28 @@ let word = 'tryber';
 let reverse = ""
 
 for (let i = 0; i < word.length; i += 1) {
-  reverse += word[word.length - 1 - i];
+  reverse += word[word.length - 1 - i]; // Solução alternativa: reverse = word.split('').reverse().join('');
 }
 
 console.log(reverse);
 
 // 3º Exercício
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+
+let maior = array[0];
+let menor = array[0];
+
+for (let i = 0; i < array.length; i += 1) {
+  if (array[i].length > maior.length) {
+    maior = array[i];
+  }
+}
+
+for (let i = 0; i < array.length; i += 1) {
+  if (array[i].length < menor.length) {
+    menor = array[i];
+  }
+}
+
+console.log(maior);
+console.log(menor);
