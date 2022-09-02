3ce3ea39137ce4dbde3f2075c786c3a15ed2024b
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-02 01:14:04 -0300

    B.3

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/bonus/script.js
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-02-javascript-array-e-loop-for/bonus/script.js
@@ -1,14 +1,40 @@
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//     if (numbers[index] < numbers[secondIndex]) {
+//       let position = numbers[index];
+//       numbers[index] = numbers[secondIndex];
+//       numbers[secondIndex] = position;
+//     }
+//   }
+// }
+
+// console.log(numbers)
+
+// let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// for (let index = 1; index < numbers.length; index += 1) {
+//   for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+//     if (numbers[index] > numbers[secondIndex]) {
+//       let position = numbers[index];
+//       numbers[index] = numbers[secondIndex];
+//       numbers[secondIndex] = position;
+//     }
+//   }
+// }
+
+// console.log(numbers)
+
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+let novo = [];
 
-for (let index = 1; index < numbers.length; index += 1) {
-  for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
-  if (numbers[index] > numbers[secondIndex]) {
-        let position = numbers[index];
-        numbers[index] = numbers[secondIndex];
-        numbers[secondIndex] = position;
-      }
-    }
+for (let index = 0; index < numbers.length; index += 1) {
+  if(index + 1 < numbers.length) {
+    novo.push(numbers[index] * numbers[index + 1]);
+  } else {
+    novo.push(numbers[index] * 2);
   }
+}
 
-  console.log(numbers)
-  
+console.log(novo);
