02d15e15ce04859593c405416fa9c1d0b5931b7a
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-02 16:09:12 -0300

    B.1

---
+++ fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-03-javascript-logica-de-programacao-e-algoritmos/bonus/script.js
@@ -0,0 +1,12 @@
+// Bonus 1
+let n = 5;
+let asterisco = '*';
+let linhas = '';
+
+for (let i = 0; i < n; i += 1) {
+  linhas += asterisco;
+}
+
+for(let i = 0; i < n; i += 1) {
+  console.log(linhas);
+}
