1f8c787885558037502d0b5bdbf028dc8fa20c75
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-05 18:04:31 -0300

    1.3

--- fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/buscando-elementos/script.js
+++ fundamentos/bloco-05-javascript-dom-eventos-e-web-storage/dia-02-javascript-trabalhando-com-elementos/buscando-elementos/script.js
@@ -2,3 +2,6 @@ const elementoOndeVoceEsta = document.getElementById('elementoOndeVoceEsta');
 
 const pai = elementoOndeVoceEsta.parentElement
 pai.style.color = 'blue';
+
+const texto = document.getElementById('primeiroFilhoDoFilho');
+texto.innerHTML = 'lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum lorem Ipsum'
