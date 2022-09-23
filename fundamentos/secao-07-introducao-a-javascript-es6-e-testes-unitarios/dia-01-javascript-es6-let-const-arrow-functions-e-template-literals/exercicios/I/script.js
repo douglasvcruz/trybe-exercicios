da04e4d3526d75464e625b83e92a199d9fdff65a
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-23 18:38:19 -0300

    2.1

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/I/script.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/I/script.js
@@ -1,12 +1,30 @@
-  const testingScope = (escopo) => {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-      console.log(ifScope);
-    } else {
-      const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
-      console.log(elseScope);
+const testingScope = (escopo) => {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora do meu escopo (else)';
+    console.log(elseScope);
+  }
+}
+
+testingScope(true);
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sortOddsAndEvens = (numbers) => {
+  for (let i = 1; i < numbers.length; i += 1) {
+    for (let j = 0; j < i; j += 1) {
+      if (numbers[i] < numbers[j]) {
+        let position = numbers[i];
+        numbers[i] = numbers[j];
+        numbers[j] = position
+      }
     }
   }
+  return numbers
+  // return `Os números ${numbers} se encontram ordenados de forma crescente!` Modo alternativo
+};
 
-  testingScope(true);
+console.log(`Os números ${sortOddsAndEvens(oddsAndEvens)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
