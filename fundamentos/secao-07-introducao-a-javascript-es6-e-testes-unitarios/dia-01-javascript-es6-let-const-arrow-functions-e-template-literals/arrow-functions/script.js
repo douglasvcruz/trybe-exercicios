641460ffe17dff240e6ae870b2123981d30271cd
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-23 00:48:32 -0300

    arrow functions

---
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/arrow-functions/script.js
@@ -0,0 +1,22 @@
+const printName = function () {
+  const myName = 'Lucas';
+  return myName;
+};
+
+console.log(printName());
+
+const printName2 = () => {
+  const myName = 'Lucas';
+  return myName;
+}
+
+console.log(printName2());
+
+const printName3 = () => 'Lucas';
+console.log(printName3());
+
+const multiplyByTwo = number => number * 2;
+console.log(multiplyByTwo(10));
+
+const multiplication = (number, multi) => number * multi;
+console.log(multiplication(8, 2));
