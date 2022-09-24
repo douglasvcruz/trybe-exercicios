30c9d2e952d764aece0e0939926bf3d9fb6dd512
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-24 18:11:32 -0300

    4.1

--- fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/II/script.js
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-01-javascript-es6-let-const-arrow-functions-e-template-literals/exercicios/II/script.js
@@ -10,3 +10,29 @@ console.log(`Esse é o fatorial ${fatorial(6)}`);
 
 const factorial = number => number > 1 ? number * factorial(number - 1) : 1;
 console.log(factorial(5));
+
+const longestWord = (frase) => {
+  const wordArray = frase.split(' ');
+  let max = 0;
+  let result = '';
+
+  for (const word of wordArray) {
+    if (word.length > max) {
+      max = word.length;
+      result = word;
+    }
+  }
+  return result;
+}
+
+console.log(longestWord('Antonio foi ao banheiro e não sabemos o que aconteceu'));
+
+const longestWord2 = text => text.split(' ').sort((a, b) => b.length - a.length)[0];
+
+console.log(longestWord2("Antonio foi ao banheiro e não sabemos o que aconteceu"));
+
+const button = document.getElementById('click');
+const contador = document.getElementById('count');
+let contagem = 0;
+
+button.addEventListener('click', () => contador.innerHTML = contagem += 1);
