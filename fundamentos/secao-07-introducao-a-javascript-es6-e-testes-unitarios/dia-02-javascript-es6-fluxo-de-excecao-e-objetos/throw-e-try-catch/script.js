4be3877cc4a339e66f2a9ca44082feb25b034c4d
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-25 00:06:39 -0300

    throw e try/catch

---
+++ fundamentos/secao-07-introducao-a-javascript-es6-e-testes-unitarios/dia-02-javascript-es6-fluxo-de-excecao-e-objetos/throw-e-try-catch/script.js
@@ -0,0 +1,42 @@
+const sum = (value1, value2) => value1 + value2;
+
+console.log(sum(2, 3));
+
+const sum2 = (value1, value2) => value1 + value2;
+
+console.log(sum2(2, '3'));
+
+const sum3 = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    return 'Os valores devem ser numéricos';
+  }
+  return value1 + value2;
+};
+
+console.log(sum3(2, '3'));
+
+const sum4 = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+  return value1 + value2;
+};
+
+// console.log(sum4(2, '3'));
+
+const verifyIsNumber = (value1, value2) => {
+  if (typeof value1 !== 'number' || typeof value2 !== 'number') {
+    throw new Error('Os valores devem ser numéricos');
+  }
+};
+
+const sum5 = (value1, value2) => {
+  try {
+    verifyIsNumber(value1, value2);
+    return value1 + value2;
+  } catch (error) {
+    return error.message;
+  }
+};
+
+console.log(sum5(2, '3'));
