59f1211b3e59d67e0d2b3e5bacff130e53a33b55
Douglas <cbdouglasvirissimo@hotmail.com>
2022-09-30 03:44:14 -0300

    Higher Order Functions

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/higher-order-functions/script.js
@@ -0,0 +1,43 @@
+const button = document.querySelector('#signup-button');
+
+const registerUser = () => {
+  console.log('Registrado com sucesso!');
+};
+
+button.addEventListener('click', registerUser);
+
+const repeat = (number, action) => {
+  for (let count = 0; count <= number; count += 1) {
+    action(count);
+  }
+};
+
+repeat(5, console.log);
+
+repeat(3, (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+});
+
+const isEven = (number) => {
+  if (number % 2 === 0) {
+    console.log(number, 'is even');
+  }
+};
+
+const isOdd = (number) => {
+  if (number % 2 == 1) {
+    console.log(number, 'is odd');
+  }
+};
+
+repeat(3, isEven);
+repeat(3, isOdd);
+
+const numberGenerator = () => {
+  return Math.random() * 100;
+}
+
+console.log(numberGenerator);
+console.log(numberGenerator());
