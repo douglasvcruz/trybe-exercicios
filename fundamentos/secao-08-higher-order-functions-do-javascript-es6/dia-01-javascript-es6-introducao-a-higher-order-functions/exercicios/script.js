6131d4d2cb628b2ce0aa0ed3eebe2673ab94955d
Douglas <cbdouglasvirissimo@hotmail.com>
2022-10-01 00:47:18 -0300

    3º Exercício

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicios/script.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-01-javascript-es6-introducao-a-higher-order-functions/exercicios/script.js
@@ -22,3 +22,25 @@ const sorteio = (number, func) => {
 }
 
 console.log(sorteio(2, oMesmo));
+
+const RIGHT_ANSWERS = ['A', 'C', 'B', 'D', 'A', 'A', 'D', 'A', 'D', 'C'];
+const STUDENT_ANSWERS = ['A', 'N.A', 'B', 'D', 'A', 'C', 'N.A', 'A', 'D', 'B'];
+
+const compare = (rightAnswer, studentAnswer) => {
+  if (rightAnswer === studentAnswer) {
+    return 1;
+  } if (studentAnswer === 'N.A') {
+    return 0;
+  }
+  return -0.5;
+};
+
+const count = (rightAnswers, studentAnswers, func) => {
+  let count = 0;
+
+  for (let i = 0; i < rightAnswers.length; i += 1) {
+    count += func(rightAnswers[i], studentAnswers[i]);
+  }
+  return `${count} Pontos!!`;
+};
+console.log(count(RIGHT_ANSWERS, STUDENT_ANSWERS, compare));
