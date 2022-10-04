93cecd84e1a2e7a960653292bfff4d63dbf7e230
Douglas <cbdouglasvirissimo@hotmail.com>
2022-10-04 00:14:53 -0300

    Object Destructuring2

---
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais-aberto-item-de-menu-com-subitens/object-destructuring/script.js
@@ -0,0 +1,68 @@
+const product = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+const { name, seller } = product;
+console.log(name); // Smart TV Crystal UHD
+console.log(seller); // Casas de Minas
+
+const character = {
+  name1: 'Luke SkyWalker',
+  age: '53',
+  description: {
+    specieName: 'Human',
+    jedi: true,
+  },
+  homeWorld: {
+    name: 'Tatooine',
+    population: '200000',
+  },
+};
+
+const { name1, age, homeWorld: { name: planetName }, description: { jedi } } = character;
+
+console.log(`Esse é o ${name}, ele tem ${age} anos, mora no planeta ${planetName} e, por incrível que possa parecer, ele ${jedi ? 'é um Jedi' : 'não é um Jedi'}.`);
+
+
+
+
+const daysOfWeek = {
+  workDays: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+  weekend: ['Saturday', 'Sunday'],
+};
+
+const { workDays, weekend } = daysOfWeek;
+console.log(workDays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+console.log(weekend); // ['Saturday', 'Sunday']
+
+const weekdays = [...workDays, ...weekend];
+console.log(weekdays); // ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+const student = {
+  a: 'Maria',
+  b: 'Turma B',
+  c: 'Matematica',
+};
+
+const { a: nome, b: classAssigned, c: subject } = student;
+
+console.log(nome); // Maria
+console.log(classAssigned); // Turma B
+console.log(subject); // Matemática
+
+const name2 = student.a;
+console.log(name2); // Maria
+
+const product1 = {
+  name: 'Smart TV Crystal UHD',
+  price: '1899.05',
+  seller: 'Casas de Minas',
+};
+
+const printProductDetails = ({ name, price, seller }) => {
+  console.log(`Promoção! ${name} por apenas ${price} é só aqui: ${seller}`);
+};
+
+printProductDetails(product1); // Promoção! Smart TV Crystal UHD por apenas 1899.05 é só aqui: Casas de Minas
