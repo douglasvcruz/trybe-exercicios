34fcd74e54c536d0488c4d6bcdba281dbe2438a8
Douglas <cbdouglasvirissimo@hotmail.com>
2022-10-04 16:37:23 -0300

    9º Exercício

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais-aberto-item-de-menu-com-subitens/exercicios/script.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais-aberto-item-de-menu-com-subitens/exercicios/script.js
@@ -114,3 +114,16 @@ const greet = (a, b = 'Hi') => `${b} ${a}`;
 console.log(greet('John'));
 console.log(greet('John', 'Good morning'));
 console.log(greet('Isabela', 'Oi'));
+
+const yearSeasons = {
+  spring: ['March', 'April', 'May'],
+  summer: ['June', 'July', 'August'],
+  autumn: ['September', 'October', 'November'],
+  winter: ['December', 'January', 'February'],
+};
+
+const { spring, summer, autumn, winter } = yearSeasons;
+
+const months = [...spring, ...summer, ...autumn, ...winter];
+
+console.log(months);
