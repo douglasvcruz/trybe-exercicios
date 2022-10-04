724ae7d4f2b53183a5e4d0e2bd2fe6e4aaa8833a
Douglas <cbdouglasvirissimo@hotmail.com>
2022-10-04 14:49:44 -0300

    2º Exercício

--- fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais-aberto-item-de-menu-com-subitens/exercicios/script.js
+++ fundamentos/secao-08-higher-order-functions-do-javascript-es6/dia-05-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais-aberto-item-de-menu-com-subitens/exercicios/script.js
@@ -8,3 +8,30 @@ const rectangles = [rectangle1, rectangle2, rectangle3];
 rectangles.forEach((rectangle) => {
   console.log(rectangleArea(...rectangle)); // Altere o parâmetro recebido por rectangleArea()
 });
+
+const sum = (...test) => test.reduce((sum, test) => sum + test, 0);
+
+console.log(sum(4, 5, 6));
+
+const alex = {
+  name: 'Alex',
+  age: 26,
+  likes: ['fly fishing'],
+  nationality: 'Australian',
+};
+
+const gunnar = {
+  name: 'Gunnar',
+  age: 30,
+  likes: ['hiking', 'scuba diving', 'taking pictures'],
+  nationality: 'Icelandic',
+};
+// name: nome da pessoa, age: idade, likes: gosta de, nationality: nacionalidade
+
+// complete a assinatura da função abaixo
+const personLikes = ({ name, age, likes }) => `${name} is ${age} years old and likes ${likes.join(', ')}.`;
+// <nome> tem <anos de idade> e gosta de <gostos da pessoa>
+
+// Retornos esperados:
+console.log(personLikes(alex)); // 'Alex is 26 years old and likes fly fishing.'
+console.log(personLikes(gunnar)); // 'Gunnar is 30 years old and likes hiking, scuba diving, taking pictures.'
