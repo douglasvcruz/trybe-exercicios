f58e6a9ba7a2b10eb157b4ddb3676b2356a135d7
Douglas <cbdouglasvirissimo@hotmail.com>
2022-10-24 21:05:36 -0300

    1º Fixar

---
+++ front-end/secao-02-introducao-ao-react/dia-01-hello-world-no-react/fixar/src/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Douglas Virissimo da Cruz</h1>
+        <p>Nascido em Canoas, Rio Grande do Sul</p>
+        <h2>Minhas Habilidades</h2>
+        <ul>
+          <li>Dinamismo</li>
+          <li>Raciocínio lógico</li>
+          <li>Criatividade</li>
+        </ul>
+      </div>
+    )
+  }
+}
