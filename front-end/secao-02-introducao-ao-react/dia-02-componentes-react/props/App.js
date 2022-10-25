593b91eec03bdcb8c6a0d4cd1025ef3b0132f5f3
Douglas <cbdouglasvirissimo@hotmail.com>
2022-10-25 17:20:37 -0300

    Props

---
+++ front-end/secao-02-introducao-ao-react/dia-02-componentes-react/props/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Greeting from './Greeting';
+
+class App extends React.Component {
+  render() {
+    return (
+      <main>
+        <Greeting name="Samuel" lastName="Silva" />
+      </main>
+    );
+  }
+}
+
+export default App;
---
+++ front-end/secao-02-introducao-ao-react/dia-02-componentes-react/props/script.js
@@ -0,0 +1,9 @@
+function greeting(name){
+  return `Hello, ${name}`;
+}
+console.log(greeting('Samuel'));
+
+function greeting2(name, lastName){
+  return `Hello, ${name} ${lastName}`;
+}
+console.log(greeting2('Samuel', 'Silva'));
