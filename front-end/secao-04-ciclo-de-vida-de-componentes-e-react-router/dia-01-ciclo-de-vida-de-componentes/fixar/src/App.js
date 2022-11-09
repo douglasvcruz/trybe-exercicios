aa2dec7123e472eecf4cd41d8311213d3c120c5e
Douglas <cbdouglasvirissimo@hotmail.com>
2022-11-09 11:51:27 -0300

    Exercícios de fixação

---
+++ front-end/secao-04-ciclo-de-vida-de-componentes-e-react-router/dia-01-ciclo-de-vida-de-componentes/fixar/src/App.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import './App.css';
+import PersonDetails from './components/PersonDetails';
+
+class App extends Component {
+  render() {
+  return (
+    <div className="App">
+    <PersonDetails />
+    </div>
+  )
+ }
+}
+export default App;
