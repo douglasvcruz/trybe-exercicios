11eaa1e40eb27fc70d3131c5043fe25c38c6df79
Douglas <cbdouglasvirissimo@hotmail.com>
2022-12-16 11:07:30 -0300

    Lendo infomações do estado global

--- front-end/secao-07-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/my-app/src/App.js
+++ front-end/secao-07-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/my-app/src/App.js
@@ -1,23 +1,22 @@
-import './App.css';
+import React from 'react';
+import { connect } from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const { countState } = this.props;
+    return (
+      <div>
+        <h1>Contador</h1>
+        <h2>{ countState }</h2>
+        <button>Incrementa 1</button>
+        <button>Incrementa 5</button>
+      </div>
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = (state) => ({
+  countState: state.count,
+});
+
+export default connect(mapStateToProps)(App);
