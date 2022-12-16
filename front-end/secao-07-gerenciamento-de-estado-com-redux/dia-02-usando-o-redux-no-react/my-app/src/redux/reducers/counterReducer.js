9a7b06ae71cd65e766958b6a06f3a56ed03fa827
Douglas <cbdouglasvirissimo@hotmail.com>
2022-12-16 12:28:18 -0300

    Imutabilidade do estado global

--- front-end/secao-07-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/my-app/src/redux/reducers/counterReducer.js
+++ front-end/secao-07-gerenciamento-de-estado-com-redux/dia-02-usando-o-redux-no-react/my-app/src/redux/reducers/counterReducer.js
@@ -1,11 +1,20 @@
 const INITIAL_STATE = {
+  clicks: 0,
   count: 0,
 };
 
 function counterReducer(state = INITIAL_STATE, action) {
   switch (action.type) {
     case 'INCREMENT_COUNTER':
-      return { count: state.count + action.payload };
+      return {
+        ...state,
+        count: state.count + action.payload,
+      };
+      case 'INCREMENT_CLICK':
+        return {
+          ...state,
+          clicks: state.clicks + 1
+        }
     default:
       return state;
   }
