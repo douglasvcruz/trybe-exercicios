3605f098cf6069bd658f34b129c92195f15024a0
Douglas <cbdouglasvirissimo@hotmail.com>
2023-03-15 18:49:24 -0300

    feat: edit movie

--- back-end/secao-04-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/exercises/src/app.js
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/exercises/src/app.js
@@ -45,8 +45,8 @@ app.get("/movies/:id", async (req, res) => {
 
 app.post("/movies", async (req, res) => {
   try {
-    const movies = await readFile();
     const { movie, price } = req.body;
+    const movies = await readFile();
     const newMovie = {
       id: movies[movies.length - 1].id + 1,
       movie,
@@ -59,5 +59,24 @@ app.post("/movies", async (req, res) => {
     res.status(500).send({ message: err.message });
   }
 });
+
+app.put('/movies/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { movie, price } = req.body;
+    const movies = await readFile();
+    const index = movies.findIndex((a) => a.id === Number(id));
+    movies[index] = {
+      id: Number(id),
+      movie,
+      price,
+    }
+    const updatedMovies = JSON.stringify(movies, null, 2);
+    await fs.writeFile(moviesPath, updatedMovies);
+    res.status(200).json(movies[index]);
+  } catch (err) {
+    res.status(500).send({ message: err.message });
+  }
+})
 
 module.exports = app;
