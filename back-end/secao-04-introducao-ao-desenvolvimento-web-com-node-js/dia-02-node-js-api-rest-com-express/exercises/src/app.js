662e99468644382b34cb3adc3951b79a04a5ddb1
Douglas <cbdouglasvirissimo@hotmail.com>
2023-03-15 17:48:06 -0300

    feat: get movies

--- back-end/secao-04-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/exercises/src/app.js
+++ back-end/secao-04-introducao-ao-desenvolvimento-web-com-node-js/dia-02-node-js-api-rest-com-express/exercises/src/app.js
@@ -4,21 +4,34 @@ const path = require("path");
 
 const app = express();
 
+app.use(express.json());
+
 const moviesPath = path.resolve(__dirname, "./movies.json");
 
 const readFile = async () => {
   try {
     const data = await fs.readFile(moviesPath);
-    return JSON.parse(data)
+    return JSON.parse(data);
   } catch (err) {
     console.log(`Arquivo não pode ser lido ${err}}`);
   }
 };
 
-const main = async () => {
-  console.log(await readFile());
-}
+// const main = async () => {
+//   console.log(await readFile());
+// };
 
-main();
+// main();
+
+app.get("/movies/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const movies = await readFile();
+    const movieFind = movies.find((a) =>  a.id === Number(id))
+    res.status(200).json(movieFind);
+  } catch (err) {
+    res.status(500).send({ message: err.message })
+  }
+});
 
 module.exports = app;
