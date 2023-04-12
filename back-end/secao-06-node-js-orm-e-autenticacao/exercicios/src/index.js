e207f18496fae8b09bf2c83184a328e744a8be91
Douglas <cbdouglasvirissimo@hotmail.com>
2023-04-12 18:05:17 -0300

    feat: eighth exercise, method get - books/:id

--- back-end/secao-06-node-js-orm-e-autenticacao/exercicios/src/index.js
+++ back-end/secao-06-node-js-orm-e-autenticacao/exercicios/src/index.js
@@ -1,10 +1,17 @@
+require('express-async-errors');
 const express = require('express');
 const BooksController = require('./controllers/book.controller');
+const { validateBookExist } = require('./middlewares/validateBook')
+const errorHandler = require('./middlewares/errorHandler');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
 app.get('/books', BooksController.getAll);
+
+app.get('/books/:id', validateBookExist, BooksController.getById);
+
+app.use(errorHandler);
 
 app.listen(PORT, () => console.log(`Ouvindo na porta ${PORT}!`));
--- back-end/secao-06-node-js-orm-e-autenticacao/exercicios/src/middlewares/validateBook.js
+++ back-end/secao-06-node-js-orm-e-autenticacao/exercicios/src/middlewares/validateBook.js
@@ -1,9 +1,9 @@
 const httpErrGenerator = require('../utils/httpErrorGenerator');
-const { User } = require('../models');
+const BooksService = require('../services/book.service')
 
 const validateBookExist = async (req, _res, next) => {
   const { id } = req.params;
-  const book = await User.findByPk(id);
+  const book = await BooksService.getById(id);
   if (!book) {
     throw httpErrGenerator(404, 'Book not found');
   }
