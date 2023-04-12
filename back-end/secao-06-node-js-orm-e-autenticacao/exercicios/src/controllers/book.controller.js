d2db9151e061a1638fa96333e2a00415bb977255
Douglas <cbdouglasvirissimo@hotmail.com>
2023-04-12 18:38:05 -0300

    fix: post

--- back-end/secao-06-node-js-orm-e-autenticacao/exercicios/src/controllers/book.controller.js
+++ back-end/secao-06-node-js-orm-e-autenticacao/exercicios/src/controllers/book.controller.js
@@ -17,7 +17,7 @@ const getById = async (req, res, next) => {
 
 const createBook = async (req, res) => {
   const { title, author, pageQuantity } = req.body;
-  const newBook = await BooksService.createBook({ title, author, pageQuantity });
+  const newBook = await BooksService.createBook(title, author, pageQuantity );
   res.status(201).json(newBook);
 };
 
