80192aa4153ed2a350a6d2a956abcb6aaed2c855
Douglas <cbdouglasvirissimo@hotmail.com>
2023-04-12 18:50:23 -0300

    feat: twelfth exercise, service - createBook

--- back-end/secao-06-node-js-orm-e-autenticacao/exercicios/src/services/book.service.js
+++ back-end/secao-06-node-js-orm-e-autenticacao/exercicios/src/services/book.service.js
@@ -18,8 +18,22 @@ const createBook = async (title, author, pageQuantity) => {
   return newBook;
 };
 
+const updateBook = async (id, { title, author, pageQuantity }) => {
+  const [updatedBook] = await User.update(
+    {
+      title,
+      author,
+      pageQuantity
+    },
+    { where: { id } },
+  );
+
+  return updatedBook;
+};
+
 module.exports = {
   getAll,
   getById,
   createBook,
+  updateBook,
 };
