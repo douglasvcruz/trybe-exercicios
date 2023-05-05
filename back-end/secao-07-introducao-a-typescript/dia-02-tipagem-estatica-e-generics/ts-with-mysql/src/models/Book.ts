87542e83558d8ae3ab02dd6083c5bba74c023265
Douglas <cbdouglasvirissimo@hotmail.com>
2023-05-05 01:34:04 -0300

    feat: model with mysql

---
+++ back-end/secao-07-introducao-a-typescript/dia-02-tipagem-estatica-e-generics/ts-with-mysql/src/models/Book.ts
@@ -0,0 +1,35 @@
+import { Pool, ResultSetHeader, RowDataPacket } from 'mysql2/promise';
+import connection from './connection';
+
+export interface Book {
+  id?: number;
+  title: string;
+  price: number;
+  author: string;
+  isbn: string;
+}
+
+export default class BookModel {
+  connection: Pool;
+
+  constructor() {
+    this.connection = connection;
+  }
+
+  async getAll(): Promise<Book[]> {
+    const result = await this.connection.execute<(Book & RowDataPacket)[]>('SELECT * FROM books');
+    const [rows] = result;
+    return rows;
+  }
+
+  async create(book: Book): Promise<Book> {
+    const { title, price, author, isbn } = book;
+
+    const [{ insertId }] = await this.connection.execute<ResultSetHeader>(
+        'INSERT INTO books (title, price, author, isbn) VALUES (?, ?, ?, ?)',
+        [title, price, author, isbn],
+    );
+
+    return { id: insertId, ...book };
+  }
+}
