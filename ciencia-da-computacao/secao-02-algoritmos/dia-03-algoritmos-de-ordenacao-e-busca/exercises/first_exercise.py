2586f02a81652eef4fcb9feef701cc080d48f134
Douglas <cbdouglasvirissimo@hotmail.com>
2023-06-10 11:23:35 -0300

    feat: bubble sort

---
+++ ciencia-da-computacao/secao-02-algoritmos/dia-03-algoritmos-de-ordenacao-e-busca/exercises/first_exercise.py
@@ -0,0 +1,9 @@
+# Bubble Sort
+
+["zebra", "macaco", "elefante", "arara", "javali"]
+
+["macaco", "elefante", "arara", "javali", "zebra"]  # Primeira iteração
+
+["elefante", "arara", "javali", "macaco", "zebra"]  # Segunda iteração
+
+["arara", "elefante", "javali", "macaco", "zebra"]  # Terceira/ultima iteração
