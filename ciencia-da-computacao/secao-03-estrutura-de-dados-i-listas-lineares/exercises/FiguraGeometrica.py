a4f28f517f85f1d493d0814cc1a8ee9e1b17cf19
Douglas <cbdouglasvirissimo@hotmail.com>
2023-06-17 13:18:45 -0300

    fix: perimeter

--- ciencia-da-computacao/secao-03-estrutura-de-dados-i-listas-lineares/exercises/FiguraGeometrica.py
+++ ciencia-da-computacao/secao-03-estrutura-de-dados-i-listas-lineares/exercises/FiguraGeometrica.py
@@ -5,3 +5,7 @@ class FiguraGeometrica(ABC):
     @abstractmethod
     def area(self):
         raise NotImplementedError
+
+    @abstractmethod
+    def perimetro(self):
+        raise NotImplementedError
